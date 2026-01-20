d181b74b76aba5cc3d351044dfe23c0d0431f020
Lokee1987 <valluruloki@gmail.com>
2026-01-20 14:29:24 +0000

    first commit changes

--- GitHubDeployPractice/Program.cs
+++ GitHubDeployPractice/Program.cs
@@ -11,7 +11,7 @@ namespace GitHubDeployPractice
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline. test change
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");

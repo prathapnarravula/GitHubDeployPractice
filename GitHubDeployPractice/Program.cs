c171b452b457bd388a7e4255ff91002a7fe89977
Lokee1987 <valluruloki@gmail.com>
2026-01-22 18:27:16 +0000

    label change

--- GitHubDeployPractice/Program.cs
+++ GitHubDeployPractice/Program.cs
@@ -12,7 +12,7 @@ namespace GitHubDeployPractice
             var app = builder.Build();
 
             // Configure the HTTP request pipeline. test change
-            if (!app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment()) 
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
